c40f78a04b92e08269a8e263056c7fc7c7ad0d2c
Samthibau <Samthibault28@gmail.com>
2023-11-22 23:46:46 -0600

    home ui change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,3 @@
-
-
 <head>
 	<title>Home</title>
 	<meta name="description" content="Explore a variety of useful web tools at Everyday Web Things. From generators, calculators, games and more, discover a world of online utilities." />
@@ -9,281 +7,258 @@
 
 
 
+  <script>
 
-<style>
-	/* Basic CSS for layout and styling */
-	.categories {
-	  display: flex;
-	  flex-direction: column;
-	  align-items: center;
-	  gap: 20px;
-	  margin-top: 20px;
-	}
+
+
+
+	onMount(() => {
+	  const script = document.createElement("script");
+	  script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4751283696014624";
+	  script.async = true;
+	  script.crossOrigin = "anonymous";
 	
-	.category {
-	  display: flex;
-	  flex-direction: column;
-	  align-items: center;
-	  gap: 10px;
-	}
+	  document.head.appendChild(script);
+	});
 	
-	.category-title {
-	  font-size: 1.2rem;
-	  margin-bottom: 5px;
-	}
 	
-	.buttons-container {
-	  display: flex;
-	  flex-wrap: wrap;
-	  justify-content: center;
-	  gap: 10px;
-	}
 	
-	.button {
-	  padding: 10px 20px;
-	  background-color: #3498db;
-	  color: white;
-	  border: none;
-	  border-radius: 5px;
-	  cursor: pointer;
-	  text-align: center;
-	  text-decoration: none;
-	  transition: background-color 0.3s ease;
-	}
+	  import AdSense from '../components/AdSense.svelte'; 
+	  import GoogleAds from '../components/GoogleAds.svelte';
 	
-	.button:hover {
-	  background-color: #2980b9;
-	}
 	
-	/* Media query for mobile devices */
-	@media (max-width: 768px) {
-	  .buttons-container {
-		display: grid;
-		grid-template-columns: repeat(2, 1fr);
-		gap: 10px;
+	
+	  let page = '/'; // Set the page path to '/' for the home page
+	
+	  // Function to track page view
+	  function trackPageView() {
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+		  dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+		gtag('config', 'G-H2EFXSCDHN', { 'page_path': page });
 	  }
-	  
-	  .button {
-		padding: 6px 12px; /* Adjusted smaller padding for smaller buttons */
-		font-size: 12px; /* Adjusted font size for smaller buttons */
-	  }
-	}
+	
+	  onMount(trackPageView);
+	
+
+
+
+    import { onMount } from 'svelte';
+  
+    let categories = [
+      {
+        name: 'Generators',
+        tools: [
+        { name: 'Random Word Generator', link: '/RandomWordGenerator', icon: '🔤' },
+        { name: 'Random Quote Generator', link: '/RandomQuoteGenerator', icon: '💬' },
+        { name: 'Random Paragraph Generator', link: '/RandomParagraphGenerator', icon: '📝' },
+        { name: 'Random Number Generator', link: '/RandomNumberGenerator', icon: '🔢' },
+        { name: 'Random Color Generator', link: '/RandomColorGenerator', icon: '🎨' },
+        { name: 'Random Name Generator', link: '/RandomNameGenerator', icon: '👤' },
+        { name: 'Dummy Image Generator', link: '/DummyImageGenerator', icon: '🖼️' },
+        { name: 'Qr Generator', link: '/QrCodeGenerator', icon: '🔲' }
+      
+          // Add more tools for productivity
+        ],
+      },
+      {
+        name: 'Productivity',
+        tools: [
+          { name: 'Timer & Stop Watch', link: '/Timer&StopWatch', icon: '⏰' },
+          { name: 'Currency Converter', link: '/CurrencyConverter', icon: '💱' },
+          { name: 'Time Zone Converter', link: '/TimeZoneConverter', icon: '⌚' },
+          { name: 'Screen Ruler', link: '/ScreenRuler', icon: '📏' },
+          { name: 'VirtualDice', link: '/VirtualDice', icon: '🎲' },
+          
+          // Add more utility tools
+        ],
+      },
+      {
+        name: 'Calculators',
+        tools: [
+            { name: 'Standard Calculator', link: '/tools/bmi-calculator', icon: '🧮' },
+            { name: 'Scientific Calculator', link: '/ScientificCalculator', icon: '🔬' },
+            { name: 'Square Root Calculator', link: '/SquareRootCalculator', icon: '√' },
+            { name: 'Percentage Calculator', link: '/PercentageCalculator', icon: '%' },
+            { name: 'Area Calculator', link: '/AreaCalculator', icon: '📐' },
+            { name: 'Volume Calculator', link: '/VolumeCalculator', icon: '⚱️' },
+            { name: 'Fraction Calculator', link: '/FractionCalculato', icon: '1/2' },
+            { name: 'Budget Calculator', link: '/BudgetCalculator', icon: '💵' }
+          // Add more health & fitness tools
+        ],
+      },
+      {
+        name: 'Education',
+        tools: [
+          { name: 'Digital Science Quiz', link: '/DigitalScienceQuiz', icon: '🧪' },
+          { name: 'Digital Math Quiz', link: '/DigitalMathQuiz', icon: '➕' },
+          { name: 'Geography Quiz', link: '/GeographyQuiz', icon: '🌍' },
+          { name: 'History Quiz', link: '/HistoryQuiz', icon: '📖' },
+          { name: 'Coding Quiz', link: '/CodingQuiz', icon: '👨‍💻' },
+          // Add more educational tools
+        ],
+      },
+      {
+        name: 'Entertainment',
+        tools: [
+         { name: 'Tic Tac Toe', link: '/TikTacToe', icon: 'ﾒ𝟶' },
+        { name: 'Joke of the Day', link: '/JokeoftheDay', icon: '😄' },
+        { name: 'Daily Fun Fact', link: '/DailyFunFact', icon: '🎉' },
+        { name: 'Daily Brain Teaser', link: '/DailyBrainTeaser', icon: '🧩' }
+          // Add more entertainment tools
+        ],
+      },
+      {
+        name: 'Health and Wellness',
+        tools: [
+            { name: 'Calorie Counter', link: '/CalorieCounter', icon: '🥗' },
+            { name: 'Random Workouts', link: '/RandomWorkouts', icon: '🏋️‍♂️' },
+            { name: 'Positive Affirmations', link: '/PositiveAffirmations', icon: '✨' }
+          // Add more travel tools
+        ],
+      },
+      
+      
+    ];
+  
+    let allCategories = [...categories]; // Save all categories initially
+  
+    let searchText = '';
+  
+    function filterCategories() {
+      if (!searchText.trim()) {
+        categories = [...allCategories]; // Show all categories when search is empty
+      } else {
+        categories = allCategories.map(category => ({
+          ...category,
+          tools: category.tools.filter(tool =>
+            tool.name.toLowerCase().includes(searchText.toLowerCase())
+          ),
+        })).filter(category => category.tools.length > 0);
+      }
+    }
+  
+    onMount(() => {
+      filterCategories(); // Initially show all categories
+    });
+  </script>
+  
+  
+  
+  <input type="text" placeholder="Search tools..." bind:value={searchText} on:input={filterCategories}>
+  
+  <div class="categories">
+    {#each categories as category}
+      <div class="category">
+        <h2>{category.name}</h2>
+        <ul>
+          {#each category.tools as tool}
+            <li>
+              <a href={tool.link}>
+                <span class="tool-icon">{tool.icon}</span>
+                <span class="tool-name">{tool.name}</span>
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </div>
+    {/each}
+  </div>
+  
+
+
+  <style>
+    /* Improved and polished CSS */
+  
+    /* Dark Mode Compatibility */
+    @media (prefers-color-scheme: dark) {
+      body {
+        background-color: #222;
+        color: #fff;
+      }
+  
+      input[type="text"],
+      a {
+        color: #000000;
+      }
+  
+      .categories {
+        border-color: #444;
+      }
+  
+      .category {
+        background-color: #333;
+        box-shadow: 0 0 10px rgba(255, 255, 255, 0.1);
+        color: #fff;
+      }
+    }
+  
+    /* Light Mode Styles */
+    h1 {
+      text-align: center;
+      font-size: 2.5rem;
+      margin-bottom: 30px;
+    }
+  
+    input[type="text"] {
+      width: 100%;
+      padding: 8px;
+      margin-bottom: 20px;
+      border-radius: 4px;
+      border: 1px solid #ccc;
+      background-color: #f9f9f9;
+      color: #333;
+    }
+  
+    .categories {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      gap: 20px;
+      border: 1px solid #ccc;
+      border-radius: 8px;
+      padding: 15px;
+    }
+  
+    /* Updated style for each category container */
+    .category {
+      flex: 0 0 calc(33.33% - 40px); /* Three items per row */
+      margin: 10px;
+      padding: 15px;
+      border: 1px solid #ccc;
+      border-radius: 8px;
+      background-color: #f9f9f9;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      transition: transform 0.3s ease-in-out;
+      color: #333;
+    }
+  
+    @media (max-width: 767px) {
+      /* Changes for mobile screens */
+      .categories {
+        flex-direction: column;
+        align-items: center;
+      }
+  
+      .category {
+        width: calc(100% - 20px);
+        flex: 0 0 calc(100% - 20px);
+      }
+    }
+  
+    .tool-name {
+      background-color: transparent !important; /* Remove background color */
+    }
+  
+    .tool-icon {
+      font-size: 1.5rem;
+      margin-right: 5px;
+      vertical-align: middle;
+    }
   </style>
   
   
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4751283696014624"
-  crossorigin="anonymous">
-
-
-import AdSense from '../components/AdSense.svelte'; 
-
-import { onMount } from 'svelte';
-
-let page = '/'; // Set the page path to '/' for the home page
-
-// Function to track page view
-function trackPageView() {
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-	dataLayer.push(arguments);
-  }
-  gtag('js', new Date());
-  gtag('config', 'G-H2EFXSCDHN', { 'page_path': page });
-}
-
-onMount(trackPageView);
-
-</script>
   
-	
-	<div class="categories">
-	<!-- Category 1 -->
-	<div class="category">
-	<h2 class="category-title">Generators</h2>
-	<div class="buttons-container">
-	<!-- Replace the '#' with the actual URLs of your website pages -->
-	<a href="/RandomWordGenerator" class="button">Random Word Generator</a>
-	<a href="/RandomQuoteGenerator" class="button">Random Quote Generator</a>
-	<a href="/RandomParagraphGenerator" class="button">Random Paragraph Generator</a>
-	<a href="/RandomNumberGenerator" class="button">Random Number Generator</a>
-	<a href="/DummyImageGenerator" class="button">Dummy Image Generator</a>
-	<a href="/RandomColorGenerator" class="button">Random Color Generator</a>
-	<a href="/QrCodeGenerator" class="button">Qr Code Generator</a>
-	<a href="/ColorPaletteGenerator" class="button">Color Palette Generator</a>
-	<a href="/RandomShakespeareanInsultGenerator" class="button">Random Shakespearean Insult Generator</a>
-	<a href="/QuizIdeaGenerator" class="button">Quiz Idea Generator</a>
-	<a href="/RandomRecipeGenerator" class="button">Random recipe Generator</a>
-	<a href="/NameGenerator" class="button">Name Generator</a>
-	<a href="/RandomCityGenerator" class="button">Random City Generator</a>
-	<a href="/StoryPlotGenerator" class="button">Story Plot Generator</a>
-	<a href="/BusinessNameGenerator" class="button">Business Name Generator</a>
-	<!-- Add more buttons with appropriate href attributes -->
-	</div>
-	</div>
-	
-	<!-- Category 2 -->
-	<div class="category">
-	<h2 class="category-title">Productivity and Organization
-	</h2>
-	<div class="buttons-container">
-<a href="/DigitalWhiteboard" class="button">DigitalWhiteboard</a>
-<a href="/BasicTranslator" class="button">Basic Translator</a>
-<a href="/URLShortener" class="button">URL Shortener</a>
-<a href="/Timer" class="button">Timer</a>
-<a href="/StopWatch" class="button">StopWatch</a>
-<a href="/DigitalCountdownCalendar" class="button">Digital Countdown Calendar</a>
-<a href="/Countdown" class="button">Countdown</a>
-<a href="/QuickPollCreator" class="button">Quick Poll Creator</a>
-<a href="/CurrencyConverter" class="button">Currency Converter</a>
-<a href="/TimeZoneConverter" class="button">Time Zone Converter</a>
-<a href="/PasswordStrengthChecker" class="button">Password Strength Checker</a>
-<a href="/TypingSpeedTest" class="button">Typing Speed Test</a>
-<a href="/ScreenRuler" class="button">Screen Ruler</a>
-
-	<!-- Add more buttons with appropriate href attributes -->
-	</div>
-	</div>
-
-
-
-
-<!-- Category 2 -->
-<div class="category">
-	<h2 class="category-title">Education
-	</h2>
-	<div class="buttons-container">
-		<a href="/DigitalScienceQuiz" class="button">Digital Science Quiz</a>
-		<a href="/DigitalMathQuiz" class="button">Digital Math Quiz</a>
-		<a href="/GeographyQuiz" class="button">Geography Quiz</a>
-		<a href="/HistoryQuiz" class="button">History Quiz</a>
-		<a href="/CodingQuiz" class="button">Coding Quiz</a>
-	<!-- Add more buttons with appropriate href attributes -->
-	</div>
-	</div>
-
-
-
-
-<!-- Category 2 -->
-<div class="category">
-	<h2 class="category-title">Calculators
-	</h2>
-	<div class="buttons-container">
-<a href="/BasicCalculator" class="button">Standard Calculator</a>
-<a href="/BudgetCalculator" class="button">Budget Calculator</a>
-<a href="/ScientificCalculator" class="button">Scientific Calculator</a>
-<a href="/SquareRootCalculator" class="button">Square Root Calculator</a>
-<a href="/PrecentageCalculator" class="button">Percentage Calculator</a>
-<a href="/AreaCalculator" class="button">Area Calculator</a>
-<a href="/VolumeCalculator" class="button">Volume Calculator</a>
-<a href="/MortgageCalculator" class="button">Mortgage Calculator</a>
-<a href="/FractionCalculator" class="button">Fraction Calculator</a>
-<a href="/TipCalculator" class="button">Tip Calculator</a>
-
-	<!-- Add more buttons with appropriate href attributes -->
-	</div>
-	</div>
-
-
-
-
-	<!-- Category 2 -->
-<div class="category">
-	<h2 class="category-title">Writing & Image tools
-	</h2>
-	<div class="buttons-container">
-		<a href="/ImageResizer" class="button">Image Resizer</a>
-<a href="/ImageConverter" class="button">Image Converter</a>
-<a href="/ImageCompressor" class="button">Image Compressor</a>
-<a href="/PlaceholderImageMaker" class="button">Placeholder Image Maker</a>
-<a href="/WordFormater" class="button">Word Formatter</a>
-<a href="/WordCounter" class="button">Word Counter</a>
-<a href="/TitleCapitlization" class="button">Title Capitalization</a>
-
-	<!-- Add more buttons with appropriate href attributes -->
-	</div>
-	</div>
-
-
-
-
-	<!-- Category 2 -->
-<div class="category">
-	<h2 class="category-title">Entertainment and Daily Content
-	</h2>
-	<div class="buttons-container">
-		<a href="/TikTacToe" class="button">Tik Tac Toe</a>
-		<a href="/JokeoftheDay" class="button">Joke of the Day</a>
-		<a href="/DailyFunFact" class="button">Daily Fun Fact</a>
-		<a href="/DailyBrainTeaser" class="button">Daily Brain Teaser</a>
-		<a href="/RealorFakeNewsQuiz" class="button">Real or Fake News Quiz</a>
-		<a href="/DigitalFortuneCookie" class="button">Digital Fortune Cookie</a>
-		<a href="/DigitalWackAMole" class="button">Digital Wack A Mole</a>
-		<a href="/VirtualDice" class="button">Virtual Dice</a>
-		
-	<!-- Add more buttons with appropriate href attributes -->
-	</div>
-	</div>
-
-
-	
-
-
-<!-- Category 2 -->
-<div class="category">
-	<h2 class="category-title">Astronomy Tools
-	</h2>
-	<div class="buttons-container">
-			<a href="/InteractivePeriodicTable" class="button">Interactive Periodic Table</a>
-			<a href="/SpaceNews" class="button">Space News</a>
-			<a href="/SpaceTriviaChallenge" class="button">Space Trivia Challenge</a>
-			<a href="/SpaceandAstronomyFacts" class="button">Space and Astronomy Facts</a>
-		  </div>
-		  
-
-	<!-- Add more buttons with appropriate href attributes -->
-	</div>
-	</div>
-
-
-
-	<!-- Category 2 -->
-<div class="category">
-	<h2 class="category-title">Travel and Adventure
-
-	</h2>
-	<div class="buttons-container">
-		<a href="/DestinationRecommender" class="button">Destination Recommender</a>
-		<a href="/TravelBudgetPlanner" class="button">Travel Budget Planner</a>
-		<a href="/TravelSafetyTips" class="button">Travel Safety Tips</a>
-		
-
-	<!-- Add more buttons with appropriate href attributes -->
-	</div>
-	</div>
-
-
-
-
-
-	<!-- Category 2 -->
-<div class="category">
-	<h2 class="category-title">Health and Wellness
-
-	</h2>
-	<div class="buttons-container">
-		<a href="/CalorieCounter" class="button">Calorie Counter</a>
-		<a href="/RandomWorkouts" class="button">Random Workouts</a>
-		<a href="/PositiveAffirmations" class="button">Positive Affirmations</a>
-		
-
-	<!-- Add more buttons with appropriate href attributes -->
-	</div>
-	</div>
-
-
-
-	
-	<!-- Add more categories in the same format -->
-	
-	
+  
